83d56ee6e3de06fde2d5f1d790e4a04534b67058
JaymeFortes <jayme.fortes@edu.pucrs.br>
2025-02-18 19:39:31 -0300

    Project create

---
+++ src/test/java/com/Desafio4_ConsultaVendas_JPQL/Desafio4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Desafio4_ConsultaVendas_JPQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
